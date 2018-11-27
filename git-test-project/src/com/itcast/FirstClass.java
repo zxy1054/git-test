f959af26218cabc973b32f63a54d044d7501b891
zhu <zhu@cn>
2018-11-27 14:47:50 +0800

    第一次提交

---
+++ git-test-project/src/com/itcast/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+
+
+    }
+}
